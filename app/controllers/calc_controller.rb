204138fb6b4827b21a78249081e12a5ea505abc8
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-07 16:35:38 +0000

    square root pages built

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -6,12 +6,22 @@ class CalcController < ApplicationController
 
   def square
 
-    render({ :template => "calc_templates/square_new"})
+    render({ :template => "calc_templates/square"})
   end
 
   def square_results
     @user_number = params.fetch("number").to_f
 
     render({ :template => "calc_templates/square_results"})
+  end
+
+  def square_root
+  
+    render({ :template => "calc_templates/square_root"})
+  end
+
+  def square_root_results
+    @user_number = params.fetch("number").to_f
+    render({ :template => "calc_templates/square_root_results"})
   end
 end
